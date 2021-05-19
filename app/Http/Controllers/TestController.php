b7741f560caa214f6b161a659b58caf76f3cf2b4
flaku2012 <72572975+flaku2012@users.noreply.github.com>
2021-05-19 22:22:58 +0200

    1

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    
+	public function controllerMethod()
+	{
+		
+		return response()->json([
+			'msg' => 'Hello my friendsss'
+		]);
+
+	}
+
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>FullStack</title>
+
+    </head>
+    <body class="antialiased">
+
+        <div id="app">
+            <mainapp></mainapp>
+        </div>
+
+    </body>
+
+    <script src="{{ asset('/js/app.js') }}"></script>
+
+</html>
